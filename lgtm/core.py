b2d8f779442ee37f40d766f5b3242e8999347dca
yshr10ic <yshr10ic@gmail.com>
2020-05-17 11:32:59 +0900

    done

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,13 +1,17 @@
 import click
 
+from lgtm.drawer import save_with_message
+from lgtm.image_source import get_image
+
+
 @click.command()
 @click.option('--message', '-m', default='LGTM', show_default=True, help='画像を乗せる文字列')
 @click.argument('keyword')
 def cli(keyword, message):
     """LGTM画像生成ツール"""
     lgtm(keyword, message)
-    click.echo('lgtm')
 
 
 def lgtm(keyword, message):
-    pass
+    with get_image(keyword) as fp:
+        save_with_message(fp, message)
